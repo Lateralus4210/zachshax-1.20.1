70b327daf41a970804f27a68fb238303907d49a4
Lateralus4210 <64373031+Lateralus4210@users.noreply.github.com>
2025-06-15 11:11:41 -0400

    20250615

---
+++ src/client/java/com/zachshax/ZachsHaxClient.java
@@ -0,0 +1,10 @@
+package com.zachshax;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ZachsHaxClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
